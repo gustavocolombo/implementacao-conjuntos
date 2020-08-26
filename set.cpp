c08dffb84bd1abae13e6646f36286269aa722a4d
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-08-26 16:19:36 -0300

    conjuntos

--- set.cpp
+++ set.cpp
@@ -18,62 +18,85 @@ set::set(unsigned int capacidade) {
 // NOTE Deve aumentar a capacidade caso necessário.
 // NOTE Observe que aumentar em apenas uma unidade não nos alivia da pressão que tínhamos antes...
 bool set::inserir(int elemento) {
-    // TODO Implementação.
 
-    for(unsigned int i = tamanho; i >= 0; i--){
-        if(elemento == vetor[elemento]){
-            return false;
+    bool elementoJaInserido = pertence(elemento);
+    
+    if(!elementoJaInserido){
+
+        if(this->tamanho < this->capacidade){
+            this->vetor[tamanho] = elemento; //caso o vetor tenha tamanho suficiente ele aloca o elemento e aumenta em 1 o tamanho
+            this->tamanho++;
+
+            return true;
         }
-    }
 
-     if(this->tamanho >= this->capacidade){
+        if(this->tamanho >= this->capacidade){
         this -> vetor = (int*) realloc (this -> vetor, sizeof(int) * (this -> capacidade * 2));
 
-       if(!this->tamanho){ //caso nao tenha tamanho suficiente abandona a execução do programa
-            return false;
-        } else {
-            this->capacidade = capacidade * 2;
-            this->vetor[tamanho] = elemento;
+            if(!this->tamanho){ //caso nao tenha tamanho suficiente abandona a execução do programa
+                return false;
+            } else {
+                this->capacidade = capacidade * 2;
+                this->vetor[tamanho] = elemento;
+                this->tamanho++;
+
+                return true;
+            }
+        }else{
+            this->vetor[tamanho] = elemento; //caso o vetor tenha tamanho suficiente ele aloca o elemento e aumenta em 1 o tamanho
             this->tamanho++;
-       }
+
+            return true;
+        }
+
     }else{
-        this->vetor[tamanho] = elemento; //caso o vetor tenha tamanho suficiente ele aloca o elemento e aumenta em 1 o tamanho
-        this->tamanho++;
+       return false;
     }
+
+    
 }
 
 // Remover "elemento" da coleção.
 // Retornar indicativo de sucesso da remoção.
 // NOTE Necessário preservar a ordem relativa dos que restarem.
 bool set::remover(int elemento) {
-    // TODO Implementação.
 
-     if(this->tamanho == 0) return false;
+    if (this->tamanho == 0){
+        return false;
+    }
 
     bool elementoPertence = pertence(elemento);
-    unsigned int indiceDoElemento = 0;
 
-
-        if(elementoPertence){ 
-            for(int i = indiceDoElemento; i < this->tamanho; i++){
-                this->vetor[i-1] = this->vetor[i];
+        if(elementoPertence){
+            for(unsigned int i = 0; i < this->tamanho; i++){
+                if(vetor[i] == elemento){
+                     for (unsigned int indiceRemovido = i; indiceRemovido < this->tamanho; indiceRemovido++) {
+                       vetor[indiceRemovido] = vetor[indiceRemovido + 1];
+                }
+                this->tamanho--;
+                return true;
             }
-            this->tamanho--;
-            return true;
         }
-
+        
+    }else{
+        return false;
+    }
     return false;
 }
 
 // Determinar se "elemento" é um dos elementos ainda na coleção.
 bool set::pertence(int elemento) const {
-    // TODO Implementação.
+
+    if(this->tamanho == 0){
+       return false;
+    }
 
     for(unsigned int i = 0; i < this->tamanho; i++){
-        if(vetor[i] == elemento){
+        if(this->vetor[i] == elemento){
             return true;
         }
     }
+
     return false;
 }
 
@@ -82,17 +105,52 @@ bool set::pertence(int elemento) const {
 
 // Tornar o próprio conjunto (`this`) o resultado de sua união com o outro informado.
 void set::uniao_com(set const &conjunto) {
-    // TODO Implementação.
 
+
+    bool elementoPertenceConjunto;
+
+        for(unsigned int conjuntoA = 0; conjuntoA < this->tamanho; conjuntoA++){
+            for(unsigned int conjuntoB = 0; conjuntoB < conjunto.tamanho; conjuntoB++){
+                if(vetor[conjuntoA] != conjunto.vetor[conjuntoB]){
+                    this->inserir(conjunto.vetor[conjuntoB]);
+
+                }
+            }
+        }
 
 }
 
 // Tornar o próprio conjunto (`this`) o resultado de sua intersecção com o outro informado.
 void set::intersecao_com(set const &conjunto) {
-    // TODO Implementação.
+    if(conjunto.tamanho == 0){
+        return;
+    }
+
+    unsigned int *vetorAuxiliar = new unsigned int[tamanho];
+
+    for(unsigned int valorA = 0; valorA < this->tamanho ; valorA++){
+        for(unsigned int valorB = 0; valorB < conjunto.tamanho; valorB++){
+            if(this->vetor[valorA] == conjunto.vetor[valorB]){
+                for(int k = 0; k < this->tamanho; k++){
+                    vetorAuxiliar[k] = this->vetor[valorA];
+                }
+            }
+        }
+    }
+
 }
 
 // Testar se este conjunto (`this`) está contido no outro informado.
 bool set::esta_contido_em(set const &conjunto) const {
-    // TODO Implementação.
+
+    if (conjunto.tamanho == 0) {
+        return true;
+    }
+
+        for(unsigned int i = 0; i < this->tamanho; i++){
+            if(pertence(conjunto.vetor[i])){
+                return true;
+            }
+        }
+    return false;
 }
